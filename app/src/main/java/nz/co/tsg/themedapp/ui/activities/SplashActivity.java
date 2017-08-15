102b3a159ad715b25d48e32821d90d6fc2ff477b
jeremyvillanueva <jeremy.g.villanueva@gmail.com>
2017-08-16 08:33:42 +1200

    add Mobile Center SDK and analytics

--- app/src/main/java/nz/co/tsg/themedapp/ui/activities/SplashActivity.java
+++ app/src/main/java/nz/co/tsg/themedapp/ui/activities/SplashActivity.java
@@ -8,12 +8,19 @@ import nz.co.tsg.themedapp.ThemedApplication;
 import nz.co.tsg.themedapp.R;
 import nz.co.tsg.themedapp.ui.Branding;
 
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        MobileCenter.start(getApplication(), "c2ff57ed-55b1-48f9-886c-85371ca57d59",
+                Analytics.class, Crashes.class);
 
         Branding branding = new Branding(this);
         ThemedApplication app = (ThemedApplication) getApplication();
